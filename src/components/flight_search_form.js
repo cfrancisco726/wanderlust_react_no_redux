53bdc76fe6d16b7a010074aee03adf2b98114a32
cfrancisco726 <cfrancisco726@gmail.com>
2017-07-03 22:59:53 -0400

    Handling User Events

---
+++ src/components/flight_search_form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FlightSearch extends Component {
+	render() {
+		return <input />;
+	}
+}
+
+export default FlightSearch;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new componenet. This component shoudl produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import FlightSearch from './components/flight_search_form';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyCEodSzpD3t7d7_Bvk076631LTmITGVKfs	'
+
+const App = () => {
+	return (
+		<div>
+			<FlightSearch />
+		</div>
+	);
+}
+// same as below. fat arrow is equal to function
+// const App = function() {
+// 	return <div>Hi!</div>;
+// }
+
+
+// Take this comment's generated HTML and put it 
+// on the page(in the DOM)
+ReactDom.render(<App />, document.querySelector('.container'));
+
+// one component per file
