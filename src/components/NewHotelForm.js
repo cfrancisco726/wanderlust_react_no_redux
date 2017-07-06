0734c0c482be2391748726879f0aa355bc51649b
cfrancisco726 <cfrancisco726@gmail.com>
2017-07-06 19:12:34 -0400

    working on hotel beds

--- src/components/NewHotelForm.js
+++ src/components/NewHotelForm.js
@@ -1,51 +1,47 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component, PropTypes}  from 'react';
+import Request                        from 'superagent';
 
-const API_KEY = 'AIzaSyCEodSzpD3t7d7_Bvk076631LTmITGVKfs';
-var API = require('qpx-express');
+const API_KEY = 'tt373n7gnkmqfweypk45tzrg';
 
 var apiKey = API_KEY;
-var qpx = new API(apiKey);
 
-class NewTripForm extends Component {
+class NewHotelForm extends Component {
   constructor(){
     super();
     this.state = {
       budget: 0,
-      departure_date: '',
-      arrival_date: '',
-      numOfGuests: '',
-      origin: '',
-      flights: []
+      checkIn: '',
+      checkOut: '',
+      rooms: 0,
+      longitude: 0,
+      latitude: 0
     }
     this.handleSubmit = this.handleSubmit.bind(this)
 
   }
 
-  reqBody(origin, departure_date, arrival_date, numOfGuests, budget) {
+  reqBody(budget, checkIn, checkOut, rooms, adults, children, longitude, latitude) {
     return({"request": {
-          "passengers": { "adultCount": parseInt(`${numOfGuests}`) },
-          "slice": [{
-              "origin": `${origin}`,
-              "destination": 'LAX',
-              "date": `${departure_date}`,
-              "maxStops": 0,
-            },
-            {
-              "origin": 'LAX',
-              "destination": `${origin}`,
-              "date": `${arrival_date}`
-            }
-          ],
-          "maxPrice": `USD${budget}`,
-          "solutions": "5"
-        }
-      }
-    )
-    };
+        "stay": {
+          "checkIn": `${checkIn}`,
+          "checkOut": `${checkOut}`,
+          "shiftDays": "2"
+        },
+        "occupancies": [
+          {
+            "rooms": `${rooms}`,
+            "adults": `${adults}`,
+            "children": `${children}`,
+          }
+        ],
+        "geolocation": {
+          "longitude": -2.021484375,
+          "latitude": 45.37680856570233 }
+    }
 
   handleSubmit(e){
     e.preventDefault()
-    qpx.getInfo(this.reqBody(this.state.origin, this.state.departure_date, this.state.arrival_date, this.state.numOfGuests, this.state.budget), function(error, flights){
+    qpx.getInfo(this.reqBody(this.state.budget, this.state.checkIn, this.state.checkOut, this.state.adults, this.state.children), function(error, flights){
 
     let trips = []
 
@@ -65,19 +61,37 @@ class NewTripForm extends Component {
     })
     console.log(trips)
   })
-	}
+  }
 
+
+        fetch('https://api.test.hotelbeds.com/hotel-api/1.0/hotels',
+        {
+          method: 'POST',
+          headers: {
+            'Accept-Encoding': 'gzip'
+            'Accept': 'application/json'
+            'Api-Key': 'tt373n7gnkmqfweypk45tzrg'
+            'X-Signature': '6d50a9de3e8672cc0031bc380700e267902cbb29a622e8a78dbd0a2ab619a959'
+            'X-Originating-Ip': '38.88.254.234'
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            grant_type: 'password',
+            username: 'user_name',
+            password: "user_password"
+          })
+        })
 
 
 
   render() {
-	return (
-		<form onSubmit={this.handleSubmit.bind(this)}>
-			budget:
-			<input type='numeric' onChange={(e)=>{this.setState({budget: e.target.value})}}/><br />
-			Number of Guests:
-			<input type='numeric' onChange={(e)=>this.setState({numOfGuests: e.target.value})}/><br />
-			Origin:
+  return (
+    <form onSubmit={this.handleSubmit.bind(this)}>
+      budget:
+      <input type='numeric' onChange={(e)=>{this.setState({budget: e.target.value})}}/><br />
+      Number of Guests:
+      <input type='numeric' onChange={(e)=>this.setState({numOfGuests: e.target.value})}/><br />
+      Origin:
 			<input type='text' onChange={(e)=>this.setState({origin: e.target.value})}/><br />
 			Departure Date:
 			<input type='date' onChange={(e)=>this.setState({departure_date: e.target.value})}/><br />
