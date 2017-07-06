c8cfa66b4cd6610467bb3fb4e56a03a5553580f6
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-07-06 19:13:17 -0400

    adding bootstrap and CSS to components - having WebPack errors

---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export class Logo extends React.Component{
+  render(){
+    return(
+      <div className="jumbotron">
+  <h1 className="center logo"><img src="../style/img/logo_hp.png" alt="Wanderlust"></img></h1>
+  <p className="center"> Your next adventure is just a budget away</p>
+  </div>
+
+    )
+  }
+} 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import NewTripForm from "./components/NewTripForm"
+import Logo from "./components/logo"
 
 // import FlightSearch from './components/flight_search_form';
 
@@ -61,10 +62,14 @@ class App extends Component {
 
 	render() {
 	return (
+		<div>
+		<Logo />
 		<NewTripForm />
-	);
+		</div>
+		);
 }
 }
+
 // same as below. fat arrow is equal to function
 // const App = function() {
 // 	return <div>Hi!</div>;
@@ -73,6 +78,5 @@ class App extends Component {
 
 // Take this comment's generated HTML and put it
 // on the page(in the DOM)
-ReactDom.render(<App />, document.querySelector('.container'));
+ReactDom.render(<App />, document.querySelector('.container'))
 
-// one component per file
