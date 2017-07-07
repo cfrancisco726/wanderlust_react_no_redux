fa81eeb53c56525399cab8a442e204a2a8c1bdff
cfrancisco726 <cfrancisco726@gmail.com>
2017-07-07 01:55:07 -0400

    update componentWillMount for hotel api but still not working

--- src/components/NewHotelList.js
+++ src/components/NewHotelList.js
@@ -17,6 +17,14 @@ class NewHotelForm extends Component {
     }
   }
 
+  var budget = 1000;
+  var checkIn = "2017-07-08";
+  var checkOut = "2017-07-10";
+  var numRooms = 1;
+  var numTravelers = 1;
+  var longitude = -2.021484375;
+  var latitude = 45.37680856570233;
+
   reqBody(budget, checkIn, checkOut, numRooms, numTravelers,  longitude, latitude) {
     return({"request": {
         "stay": {
@@ -31,8 +39,8 @@ class NewHotelForm extends Component {
           }
         ],
         "geolocation": {
-          "longitude": -2.021484375,
-          "latitude": 45.37680856570233,
+          "longitude": `${longitude}`,
+          "latitude": `${latitude}`,
           "radius": 20,
           "unit": "mi"
         }
@@ -40,6 +48,7 @@ class NewHotelForm extends Component {
   };
 
   componentWillMount(){
+    debugger;
     var url = "https://api.test.hotelbeds.com/hotel-api/1.0/hotels";
       Request.post(url)
         .set('Accept-Encoding': 'gzip')
@@ -50,53 +59,49 @@ class NewHotelForm extends Component {
         .set('Content-Type': 'application/json')
         .send(this.reqBody(this.state.budget, this.state.checkIn, this.state.checkOut, this.state.numRooms, this.state.numTravelers,  this.state.longitude, this.state.latitude)
         .end(function(err, res){
-          console.log(res)
+        console.log(res)
       });
+    }
 
-      // method: 'POST',
-      // headers: {
-      //   'Accept-Encoding': 'gzip'
-      //   'Accept': 'application/json'
-      //   'Api-Key': 'tt373n7gnkmqfweypk45tzrg'
-      //   'X-Signature': '1bd54e832e1bd2d470f6f228ad1971f5032755cdd2006ffe35b7599062008642'
-      //   'X-Originating-Ip': '73.112.72.225'
-      //   'Content-Type': 'application/json'
-      // },
-      // body: JSON.stringify(this.reqBody(this.state.budget, this.state.checkIn, this.state.checkOut, this.state.numRooms, this.state.numTravelers,  this.state.longitude, this.state.latitude), (function(hotels){
-        
-        let hotels = []
-      })
-        console.log(hotels);
+  // componentWillMount(){
+  //   debugger;
+  //   var url = "https://api.test.hotelbeds.com/hotel-api/1.0/hotels";
+  //     Request.post(url)
+  //       .set('Accept-Encoding': 'gzip')
+  //       .set('Accept': 'application/json')
+  //       .set('Api-Key': 'tt373n7gnkmqfweypk45tzrg')
+  //       .set('X-Signature': '1bd54e832e1bd2d470f6f228ad1971f5032755cdd2006ffe35b7599062008642')
+  //       .set('X-Originating-Ip': '73.112.72.225')
+  //       .set('Content-Type': 'application/json')
+  //       .send(this.reqBody(this.state.budget, this.state.checkIn, this.state.checkOut, this.state.numRooms, this.state.numTravelers,  this.state.longitude, this.state.latitude)
+  //       .then(res) => {
+  //         this.setState({
+  //           hotels: res
+  //         });
+  //       console.log(res)
+  //     });
+  //   }
+
+  componentDidMount(){
+
   }
 
+  componentWillReceiveProps(nextProps){
 
-// reqBody(budget, checkIn, checkOut, numRooms, numAdults, numChildren, longitude, latitude) {
-//     return({"request": {
-//         "stay": {
-//           "checkIn": `${checkIn}`,
-//           "checkOut": `${checkOut}`,
-//         },
-//         "occupancies": [
-//           {
-//             "rooms": `${numRooms}`,
-//             "adults": `${numAdults}`,
-//             "children": `${numChildren}`,
-//           }
-//         ],
-//         "geolocation": {
-//           "longitude": -2.021484375,
-//           "latitude": 45.37680856570233,
-//           "radius": 20,
-//           "unit": "mi"
-//         }
-//     }
+  }
 
+  componentWillUpdate(nextProps, nextState){
+
+  }
+
+  componentWillUnmount(){
+
+  }
+  
   render() {
-    return(
-      )
+    return <div>
+      </div>;
   }
 
-
-}
 
 export default NewHotelList
