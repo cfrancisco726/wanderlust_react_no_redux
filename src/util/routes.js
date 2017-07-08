98cca5a2f3d772d4c594f2ba3a3863f50fb64f48
Daniel Velez <email": null>
2017-07-08 11:21:20 -0400

    made changes

--- src/util/routes.js
+++ src/util/routes.js
@@ -1,14 +1,48 @@
-// import React from 'react';
-// import {Route, IndexRoute} from 'react-router';
-// import Index from '../index';
-// import SignupPage from '../components/signup/SignupPage'
-// import NewTripForm from '../components/NewTripForm'
-//
-// export default (
-//   <Route path="/" component={Index}>
-//   <IndexRoute component={NewTripForm} />
-//   <Route path="signup" component={SignupPage} />
-//
-//
-//   </Route>
-// )
+import Base from '../components/Base.jsx';
+import HomePage from '../components/HomePage.jsx';
+import App from '../components/App';
+import LoginPage from '../components/LoginPage';
+import SignUpPage from '../components/SignUpPage';
+import Auth from '../modules/Auth';
+
+
+const routes = {
+
+  component: Base,
+  childRoutes: [
+
+    {
+      path: '/',
+      getComponent: (location, callback) => {
+        if (Auth.isUserAuthenticated()) {
+          callback(null, App);
+        } else {
+          callback(null, HomePage);
+        }
+      }
+    },
+
+    {
+      path: '/login',
+      component: LoginPage
+    },
+
+    {
+      path: '/signup',
+      component: SignUpPage
+    },
+
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        Auth.deauthenticateUser();
+
+        // change the current URL to /
+        replace('/');
+      }
+    }
+
+  ]
+};
+
+export default routes;
